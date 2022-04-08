6fc590e5af3efbee6a20c368e8d63fd5f19cfd0f
Jakub Kicinski <kuba@kernel.org>
2022-04-08 11:31:27 -0700

    tls: rx: simplify async wait
    commit 37943f047bfb88ba4dfc7a522563f57c86d088a0 upstream.
    
    Since we are protected from async completions by decrypt_compl_lock
    we can drop the async_notify and reinit the completion before we
    start waiting.
    
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Stable-dep-of: aec7961916f3 ("tls: fix race between async notify and socket close")
    Signed-off-by: Shaoying Xu <shaoyi@amazon.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    (cherry picked from commit 704402f913b809345b2372a05d7822d03f29f923)

--- include/net/tls.h
+++ include/net/tls.h
@@ -156,7 +156,6 @@ struct tls_sw_context_rx {
 	atomic_t decrypt_pending;
 	/* protect crypto_wait with decrypt_pending*/
 	spinlock_t decrypt_compl_lock;
-	bool async_notify;
 
 	ANDROID_KABI_RESERVE(1);
 };
