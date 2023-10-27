a13ca70a6982199918690d84ab374b106a4f1801
Hardik Gajjar <hgajjar@de.adit-jv.com>
2023-10-27 17:20:28 +0200

    usb: xhci: Add timeout argument in address_device USB HCD callback
    [ Upstream commit a769154c7cac037914ba375ae88aae55b2c853e0 ]
    
    - The HCD address_device callback now accepts a user-defined timeout value
      in milliseconds, providing better control over command execution times.
    - The default timeout value for the address_device command has been set
      to 5000 ms, aligning with the USB 3.2 specification. However, this
      timeout can be adjusted as needed.
    - The xhci_setup_device function has been updated to accept the timeout
      value, allowing it to specify the maximum wait time for the command
      operation to complete.
    - The hub driver has also been updated to accommodate the newly added
      timeout parameter during the SET_ADDRESS request.
    
    Signed-off-by: Hardik Gajjar <hgajjar@de.adit-jv.com>
    Reviewed-by: Mathias Nyman <mathias.nyman@linux.intel.com>
    Link: https://lore.kernel.org/r/20231027152029.104363-1-hgajjar@de.adit-jv.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Stable-dep-of: 1e0a19912adb ("usb: xhci: Fix NULL pointer dereference on certain command aborts")
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    (cherry picked from commit b27f10764d800d6875fef8649099bacaba15b0bf)

--- drivers/usb/host/xhci.h
+++ drivers/usb/host/xhci.h
@@ -574,6 +574,9 @@ struct xhci_command {
 
 	ANDROID_KABI_RESERVE(1);
 	ANDROID_KABI_RESERVE(2);
+
+	/* xHCI command response timeout in milliseconds */
+	unsigned int			timeout_ms;
 };
 
 /* drop context bitmasks */
@@ -1338,8 +1341,11 @@ struct xhci_td {
 	unsigned int		num_trbs;
 };
 
-/* xHCI command default timeout value */
-#define XHCI_CMD_DEFAULT_TIMEOUT	(5 * HZ)
+/*
+ * xHCI command default timeout value in milliseconds.
+ * USB 3.2 spec, section 9.2.6.1
+ */
+#define XHCI_CMD_DEFAULT_TIMEOUT	5000
 
 /* command descriptor */
 struct xhci_cd {
